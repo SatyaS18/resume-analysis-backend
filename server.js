00c3c7f5de9bcda285ec5884e6922d450a369c09
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2025-02-21 20:41:16 +0530

    add another route

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ connectDB();
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/resume", require("./routes/resumeRoutes"));
 app.use("/api/search", require("./routes/searchRoutes"));
+app.get("/", (req, res) => {
+  res.send("Resume Analysis API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
